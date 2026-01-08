62d1c3eee9a9e5e44f6d97457582992e49c68a57
Abdul Rafay Kharadi <abdulrafaykharadi@gmail.com>
2026-01-08 19:43:39 +0500

    Add Cisco JavaScript Essentials 1 Exercises 2.0.15 files

---
+++ 2.0.15_lab_01-jan-2026.js
@@ -0,0 +1,14 @@
+let user1 = "Maxwell Wright";
+let user2 = "Raja Villarreal";
+let user3 = "Helen Richards";
+
+let user1Ph = "(0191) 719 6495";
+let user2Ph = "08663982895";
+let user3Ph = "08001111";
+
+let user1Email = "Curabitur.egestas.nunc@nonnumyac.co.uk";
+let user2Email = "posuere.vulputate@sed.com";
+let user3Email = "libero@convallis.edu";
+
+console.log("user/phone/email:", user1 , "/" , user1Ph , "/" ,user1Email);
+console.log("user/phone/email:", user3 , "/" , user3Ph , "/" ,user3Email);
